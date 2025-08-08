aba661ebd8f92453e65e35b212700e99f8a34492
Raymon2804 <raymonbrighk@gmail.com>
2025-08-08 22:41:52 +0530

    First commit

---
+++ routes/student.js
@@ -0,0 +1,38 @@
+const express=require("express");
+const router=express.Router();
+const student=require("../models/student");
+const course=require("../models/courses");
+const enrollment = require("../models/enrollment");
+
+//enrolled msg...
+router.post("/enrolled", async (req, res) => {
+    const { username } = req.body;
+    const students = await student.findOne({ username });
+    await enrollment.create(req.body);
+    res.render("enroll.ejs", { students });
+});
+
+//view all the courses
+router.get("/course/:username",async(req,res)=>{
+    const {username}=req.params;
+    const courses= await course.find();
+    const students= await student.findOne({username});
+    console.log("students:",students);
+    res.render("courses.ejs",{students,courses,role:"student"});
+})
+//login page
+router.get("/login",(req,res)=>{
+    res.render("login.ejs",{role:"student"});
+})
+//authenticating the user
+router.post("/login",async(req,res)=>{
+    const{username,password}=req.body;
+    const user= await student.findOne({username});
+    if(user && user.password === password){
+        res.redirect(`/student/course/${username}`);
+    }else{
+        res.send("check your credentials");
+    }
+})
+
+module.exports=router;
